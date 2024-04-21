383fa667c5195742c099974a2be8470e0d56e77b
hong le <vuanhde@yahoo.de>
2024-04-21 20:25:20 +0200

    4b.commit am 21.04.24

--- src/main/java/hong/demo/config/AppConfig.java
+++ src/main/java/hong/demo/config/AppConfig.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
-// @Import({GirlConfig.class,})
+// @Import({BeanConfig.class,})
 @Configuration
 @ComponentScan(basePackages = {"hong.demo.service"})
 public class AppConfig {
